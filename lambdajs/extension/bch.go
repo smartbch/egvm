1e0447b55422b38dbf9f3d98ef0337280b5f5e46
zxh0 <zhangxiuhong2013@gmail.com>
2023-04-04 11:42:33 +0800

    Implement ParseTxInHex

--- lambdajs/extension/bch.go
+++ lambdajs/extension/bch.go
@@ -1,5 +1,13 @@
 package extension
 
+import (
+	"bytes"
+	"encoding/hex"
+
+	"github.com/gcash/bchd/txscript"
+	"github.com/gcash/bchd/wire"
+)
+
 type BchTx struct {
 	HexTxID  string
 	Version  int32
@@ -21,12 +29,74 @@ type OutPoint struct {
 
 type TxOut struct {
 	Value           int64
-	HexPubkeyHash   string // for P2PKH
+	HexPubkeyHash   string   // for P2PKH
 	HexDataElements []string // the pushed data in OP_RETURN, empty when it's P2PKH
 }
 
-func ParseTxInHex(hex string) BchTx {
-	return BchTx{} //TODO
+func ParseTxInHex(hexStr string) BchTx {
+	data, err := hex.DecodeString(hexStr)
+	if err != nil {
+		panic(err) // TODO: return error
+	}
+
+	msgTx := &wire.MsgTx{}
+	err = msgTx.Deserialize(bytes.NewReader(data))
+	if err != nil {
+		panic(err) // TODO: return error
+	}
+
+	txIn := make([]TxIn, len(msgTx.TxIn))
+	for i, msgTxIn := range msgTx.TxIn {
+		txIn[i] = TxIn{
+			PreviousOutPoint: OutPoint{
+				HexTxID: msgTxIn.PreviousOutPoint.Hash.String(),
+				Index:   msgTxIn.PreviousOutPoint.Index,
+			},
+			//HexPubkey: ,
+			Sequence: msgTxIn.Sequence,
+		}
+	}
+
+	txOut := make([]TxOut, len(msgTx.TxOut))
+	for i, msgTxOut := range msgTx.TxOut {
+		txOut[i] = TxOut{
+			Value:           msgTxOut.Value,
+			HexPubkeyHash:   getPubkeyHashHex(msgTxOut.PkScript),
+			HexDataElements: getOpRetData(msgTxOut.PkScript),
+		}
+	}
+
+	return BchTx{
+		HexTxID:  msgTx.TxHash().String(),
+		Version:  msgTx.Version,
+		TxIn:     txIn,
+		TxOut:    txOut,
+		LockTime: msgTx.LockTime,
+	}
+}
+
+// OP_DUP OP_HASH160 <pkh> OP_EQUALVERIFY OP_CHECKSIG
+func getPubkeyHashHex(pkScript []byte) string {
+	if len(pkScript) == 25 &&
+		pkScript[0] == txscript.OP_DUP &&
+		pkScript[1] == txscript.OP_HASH160 &&
+		pkScript[23] == txscript.OP_EQUALVERIFY &&
+		pkScript[24] == txscript.OP_CHECKSIG {
+
+		return hex.EncodeToString(pkScript[3:23])
+	}
+	return ""
+}
+
+func getOpRetData(pkScript []byte) (hexData []string) {
+	if len(pkScript) > 1 && pkScript[0] == txscript.OP_RETURN {
+		data, _ := txscript.PushedData(pkScript)
+		hexData = make([]string, len(data))
+		for i, item := range data {
+			hexData[i] = hex.EncodeToString(item)
+		}
+	}
+	return
 }
 
 func SignTxAndSerialize(tx BchTx, minerFeePrice float64, privateKeys ...PrivateKey) string {
---
+++ lambdajs/extension/bch_test.go
@@ -0,0 +1,25 @@
+package extension
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetPubkeyHashHex(t *testing.T) {
+	pkScript, _ := hex.DecodeString("76a914307f40d73e01af33364901d82d5614e370f905d388ac")
+	pbkHash := getPubkeyHashHex(pkScript)
+	require.Equal(t, "307f40d73e01af33364901d82d5614e370f905d3", pbkHash)
+}
+
+func TestGetOpRetData(t *testing.T) {
+	pkScript, _ := hex.DecodeString("6a04454754581456eb561cb6f98a985f80464fa99267a462c91bdb14e94358e473941de2d75d19fa330d607e05ffab4214efc507fb38cbcae3b32d1777e54593bc07eca5a1204ea5c508a6566e76240543f8feb06fd457777be300005af3107a400000000001")
+	retData := getOpRetData(pkScript)
+	require.Len(t, retData, 5)
+	require.Equal(t, retData[0], "45475458")
+	require.Equal(t, retData[1], "56eb561cb6f98a985f80464fa99267a462c91bdb")
+	require.Equal(t, retData[2], "e94358e473941de2d75d19fa330d607e05ffab42")
+	require.Equal(t, retData[3], "efc507fb38cbcae3b32d1777e54593bc07eca5a1")
+	require.Equal(t, retData[4], "4ea5c508a6566e76240543f8feb06fd457777be300005af3107a400000000001")
+}

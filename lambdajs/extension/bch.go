1ddb8fad4819080b889c13760df666c2ac59517d
zxh0 <zhangxiuhong2013@gmail.com>
2023-04-04 14:50:56 +0800

    Implement SignTxAndSerialize

--- lambdajs/extension/bch.go
+++ lambdajs/extension/bch.go
@@ -3,9 +3,13 @@ package extension
 import (
 	"bytes"
 	"encoding/hex"
+	"fmt"
 
+	"github.com/gcash/bchd/bchec"
+	"github.com/gcash/bchd/chaincfg/chainhash"
 	"github.com/gcash/bchd/txscript"
 	"github.com/gcash/bchd/wire"
+	"github.com/gcash/bchutil"
 )
 
 type BchTx struct {
@@ -33,16 +37,16 @@ type TxOut struct {
 	HexDataElements []string // the pushed data in OP_RETURN, empty when it's P2PKH
 }
 
-func ParseTxInHex(hexStr string) BchTx {
+func ParseTxInHex(hexStr string) (*BchTx, error) {
 	data, err := hex.DecodeString(hexStr)
 	if err != nil {
-		panic(err) // TODO: return error
+		return nil, fmt.Errorf("failed to decode hex string: %w", err)
 	}
 
 	msgTx := &wire.MsgTx{}
 	err = msgTx.Deserialize(bytes.NewReader(data))
 	if err != nil {
-		panic(err) // TODO: return error
+		return nil, fmt.Errorf("failed to deserialize tx: %w", err)
 	}
 
 	txIn := make([]TxIn, len(msgTx.TxIn))
@@ -52,7 +56,7 @@ func ParseTxInHex(hexStr string) BchTx {
 				HexTxID: msgTxIn.PreviousOutPoint.Hash.String(),
 				Index:   msgTxIn.PreviousOutPoint.Index,
 			},
-			//HexPubkey: ,
+			//HexPubkey: , // TODO
 			Sequence: msgTxIn.Sequence,
 		}
 	}
@@ -66,13 +70,13 @@ func ParseTxInHex(hexStr string) BchTx {
 		}
 	}
 
-	return BchTx{
+	return &BchTx{
 		HexTxID:  msgTx.TxHash().String(),
 		Version:  msgTx.Version,
 		TxIn:     txIn,
 		TxOut:    txOut,
 		LockTime: msgTx.LockTime,
-	}
+	}, nil
 }
 
 // OP_DUP OP_HASH160 <pkh> OP_EQUALVERIFY OP_CHECKSIG
@@ -99,6 +103,96 @@ func getOpRetData(pkScript []byte) (hexData []string) {
 	return
 }
 
-func SignTxAndSerialize(tx BchTx, minerFeePrice float64, privateKeys ...PrivateKey) string {
-	return "" //TODO
+func SignTxAndSerialize(tx BchTx, minerFeePrice float64, privateKeys ...PrivateKey) (string, error) {
+	if len(tx.TxIn) != len(privateKeys) {
+		return "", fmt.Errorf("length of tx inputs and private keys mismatch")
+	}
+
+	msgTx := wire.NewMsgTx(tx.Version)
+	msgTx.LockTime = tx.LockTime
+	for idx, in := range tx.TxIn {
+		prevTxHash, err := chainhash.NewHashFromStr(in.PreviousOutPoint.HexTxID)
+		if err != nil {
+			return "", fmt.Errorf("failed to decode prevTxHash of input#%d: %w", idx, err)
+		}
+		msgTx.AddTxIn(&wire.TxIn{
+			PreviousOutPoint: wire.OutPoint{
+				Hash:  *prevTxHash,
+				Index: in.PreviousOutPoint.Index,
+			},
+		})
+	}
+	for idx, out := range tx.TxOut {
+		pubkeyHash, err := hex.DecodeString(out.HexPubkeyHash)
+		if err != nil {
+			return "", fmt.Errorf("failed to decode pubkeyHash of output#%d, %w", idx, err)
+		}
+		pkScript, err := payToPubKeyHashScript(pubkeyHash)
+		if err != nil {
+			return "", fmt.Errorf("failed to create pkScript of output#%d, %w", idx, err)
+		}
+		msgTx.AddTxOut(&wire.TxOut{
+			Value:    out.Value,
+			PkScript: pkScript,
+		})
+	}
+
+	sigHashes := txscript.NewTxSigHashes(msgTx)
+	hashType := txscript.SigHashAll | txscript.SigHashForkID
+	for idx, in := range msgTx.TxIn {
+		privKey := (*bchec.PrivateKey)(privateKeys[idx].toECDSA())
+		pubKeyBytes := privKey.PubKey().SerializeCompressed()
+		pubkeyHash := bchutil.Hash160(pubKeyBytes)
+
+		pubkeyScript, err := payToPubKeyHashScript(pubkeyHash)
+		if err != nil {
+			return "", fmt.Errorf("failed to create locaking script of input#%d, %w", idx, err)
+		}
+
+		var inputVal int64 = 0 // TODO
+		sigHash, err := txscript.CalcSignatureHash(pubkeyScript, sigHashes, hashType, msgTx, idx, inputVal, true)
+		if err != nil {
+			return "", fmt.Errorf("failed to calc signature hash of input#%d, %w", idx, err)
+		}
+
+		sig, err := signTxSigHashECDSA(privKey, sigHash, hashType)
+		if err != nil {
+			return "", fmt.Errorf("failed to sign sigHash of input#%d, %w", idx, err)
+		}
+
+		sigScript, err := txscript.NewScriptBuilder().AddData(sig).AddData(pubKeyBytes).Script()
+		if err != nil {
+			return "", fmt.Errorf("failed to create unlocking script of input#%d, %w", idx, err)
+		}
+		in.SignatureScript = sigScript
+	}
+
+	return hex.EncodeToString(msgTxToBytes(msgTx)), nil
+}
+
+func payToPubKeyHashScript(pubKeyHash []byte) ([]byte, error) {
+	return txscript.NewScriptBuilder().
+		AddOp(txscript.OP_DUP).
+		AddOp(txscript.OP_HASH160).
+		AddData(pubKeyHash).
+		AddOp(txscript.OP_EQUALVERIFY).
+		AddOp(txscript.OP_CHECKSIG).
+		Script()
+}
+
+func signTxSigHashECDSA(privKey *bchec.PrivateKey,
+	hash []byte, hashType txscript.SigHashType) ([]byte, error) {
+
+	signature, err := privKey.SignECDSA(hash)
+	if err != nil {
+		return nil, fmt.Errorf("cannot sign tx input: %s", err)
+	}
+
+	return append(signature.Serialize(), byte(hashType)), nil
+}
+
+func msgTxToBytes(tx *wire.MsgTx) []byte {
+	var buf bytes.Buffer
+	_ = tx.Serialize(&buf)
+	return buf.Bytes()
 }

a17d6cc33ff74cba0676f18dee09e3b168340bf5
helldealer <lhrmachinery@163.com>
2023-04-20 10:27:32 +0800

    Add run mode flags in script

--- egvm-script/cmd/main.go
+++ egvm-script/cmd/main.go
@@ -4,10 +4,10 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"runtime"
 	"syscall"
+	"time"
 
 	"github.com/dop251/goja"
 	"github.com/smartbch/pureauth/keygrantor"
@@ -18,38 +18,37 @@ import (
 )
 
 var privKey *bip32.Key
-var maxMemSize uint64 = 1024 * 1024 * 1024 // 1G
+var maxMemSize uint64 = 1024 * 1024 * 1024   // 1G
+var defaultRunTimeLimitInLoopMode int64 = 30 // 30s
 
 func main() {
-	var runMode string
+	var timeLimitInLoopMode int64
+	var singleMode bool
+	var perpetualMode bool
 	var keygrantorUrl string
-	flag.StringVar(&runMode, "m", "once", "run mode: once (default), keepalive")
+	flag.Int64Var(&timeLimitInLoopMode, "t", defaultRunTimeLimitInLoopMode, "enable loop mode: specific run time limit in second")
+	flag.BoolVar(&singleMode, "s", false, "enable single mode: accept one input and return one output, then process exit")
+	flag.BoolVar(&perpetualMode, "p", false, "enable perpetual mode: not clear the state after script run, accept continuous input")
 	flag.StringVar(&keygrantorUrl, "k", "127.0.0.1:8084", "keygrantor url")
 	flag.Parse()
+
 	setRlimit(maxMemSize)
+
 	var err error
 	privKey, err = keygrantor.GetKeyFromKeyGrantor(keygrantorUrl, [32]byte{})
 	if err != nil {
 		//panic(err) // comment for core logic test
 	}
-	if runMode == "keepalive" {
-		runForever()
-	} else {
-		src, err := readSource(flag.Arg(0))
-		if err != nil {
-			handleError(err)
-			os.Exit(64)
-		}
-		out, err := run(string(src))
-		if err != nil {
-			handleError(err)
-			os.Exit(64)
-		}
-		fmt.Println(out)
+	if perpetualMode {
+		executeLambdaJob(false, true, 0)
+	} else if singleMode {
+		executeLambdaJob(true, false, 0)
+	} else { // loop mode
+		executeLambdaJob(false, false, timeLimitInLoopMode)
 	}
 }
 
-func runForever() {
+func executeLambdaJob(isSingleMode bool, isPerpetualMode bool, timeLimit int64) {
 	for {
 		var job types.LambdaJob
 		err := job.DecodeMsg(msgp.NewReader(os.Stdin))
@@ -57,7 +56,7 @@ func runForever() {
 			panic(err) //todo: log it
 		}
 		// todo: get output and covert to types.LambdaResult
-		_, err = run(job.Script)
+		_, err = run(job.Script, timeLimit)
 		if err != nil {
 			handleError(err)
 			continue
@@ -68,18 +67,29 @@ func runForever() {
 		if err != nil {
 			panic(err)
 		}
-		return
+		if isSingleMode {
+			return
+		}
 	}
 }
 
-func run(script string) (goja.Value, error) {
+func run(script string, timeLimit int64) (goja.Value, error) {
 	vm := goja.New()
 	RegisterFunctions(vm)
-	result, err := vm.RunString(script)
-	if err != nil {
-		return nil, err
+	if timeLimit != 0 {
+		var closeChan = make(chan bool)
+		defer close(closeChan)
+		go func() {
+			select {
+			case <-time.After(time.Duration(timeLimit) * time.Second):
+				vm.Interrupt(errors.New("execution time exceed"))
+			case <-closeChan:
+				vm.ClearInterrupt()
+			}
+		}()
 	}
-	return result, nil
+	result, err := vm.RunString(script)
+	return result, err
 }
 
 func handleError(err error) {
@@ -89,13 +99,6 @@ func handleError(err error) {
 	} else {
 		fmt.Fprintln(os.Stderr, err)
 	}
-}
-
-func readSource(filename string) ([]byte, error) {
-	if filename == "" || filename == "-" {
-		return io.ReadAll(os.Stdin)
-	}
-	return os.ReadFile(filename) //nolint: gosec
 }
 
 func setRlimit(maxMemSize uint64) {
